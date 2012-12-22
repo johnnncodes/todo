14229915381f73161dac362826f7a854a749486d
John Kevin Basco <basco.johnkevin@gmail.com>
2012-12-22 07:34:47 -0800

    add main stylesheet

--- application/views/layouts/common.blade.php
+++ application/views/layouts/common.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset=utf-8 />
 	<title>{{ $page_title }}</title>
-	<link rel="stylesheet" type="text/css" media="screen" href="css/master.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="{{ asset('css/style.css'); }}" />
 	<!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script> -->
 	<script type="text/javascript" src="{{ asset('js/jquery-1.8.2.js'); }}"></script>
 	<!--[if IE]>
