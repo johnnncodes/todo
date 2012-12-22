1b05673f1b6a05efa2ea7fdcc2e7a5c316ac179f
John Kevin Basco <basco.johnkevin@gmail.com>
2012-12-22 07:32:52 -0800

    add local jquery-1.8.2 file

--- application/views/layouts/common.blade.php
+++ application/views/layouts/common.blade.php
@@ -5,6 +5,7 @@
 	<title>{{ $page_title }}</title>
 	<link rel="stylesheet" type="text/css" media="screen" href="css/master.css" />
 	<!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script> -->
+	<script type="text/javascript" src="{{ asset('js/jquery-1.8.2.js'); }}"></script>
 	<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
